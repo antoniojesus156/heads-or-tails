fd476cb113461200a8256ff799ec194330345ee0
Antonio Jesús <44335139+antoniojesus156@users.noreply.github.com>
2022-01-26 13:50:38 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -19,3 +19,22 @@ input.onLogoEvent(TouchButtonEvent.Released, function () {
     basic.showNumber(cross)
     basic.showIcon(IconNames.No)
 })
+input.onButtonPressed(Button.AB, function () {
+    counter = 0
+    cross = 0
+    basic.showIcon(IconNames.Diamond)
+    basic.showIcon(IconNames.SmallDiamond)
+    basic.showIcon(IconNames.Diamond)
+    basic.showIcon(IconNames.SmallDiamond)
+    for (let index = 0; index < 100; index++) {
+        if (Math.randomBoolean()) {
+            counter += 1
+        } else {
+            cross += 1
+        }
+    }
+    basic.showNumber(counter)
+    basic.showIcon(IconNames.Happy)
+    basic.showNumber(cross)
+    basic.showIcon(IconNames.No)
+})
